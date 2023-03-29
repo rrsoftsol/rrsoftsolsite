d7666edb37ba9fd9b1bdb7ab0a11efea79a9d8e3
Janga, Bhoopal <Bhoopal.Janga@ais.com>
2023-03-29 23:15:46 +0530

    update about us

--- src/pages/about-us.jsx
+++ src/pages/about-us.jsx
@@ -3,22 +3,7 @@ import Layout from "../components/layout";
 import { Container, Row, Card, Button } from 'react-bootstrap'
 
 export default function Services() {
-
-  const iconGRid = [
-
-    {
-      title: "24*7 Support",
-      desc: "Highly available teams."
-    },
-    {
-      title: "Strong Teams",
-      desc: "Strong Techincal expertise people."
-    },
-    {
-      title: "Customer Satisfaction",
-      desc: "Customer centric Aproach."
-    },
-  ];
+ 
 
   return (
     <Layout>
@@ -28,62 +13,40 @@ export default function Services() {
             <img src="/images/about.jpg" className="d-block mx-lg-auto img-fluid" alt="RR Softsol About us"></img>
           </div>
           <div className="col-lg-6">
-            <h5 className="text-crr">ABOUT RR SOFTSOL</h5>
-            <h1 className="display-3 fw-bold lh-sm mb-3">Your Digital Partner.</h1>
-            <p className="lead">We offer a range of services to meet the complex and evolving challenges that you face on multiple fronts.</p>
-            <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-              <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Contact Us</button>
-            </div>
+            <h5 className="text-crr">ABOUT RR SOFTSOL</h5> 
+            <p className="lead">We are a dynamic and innovative software company that specializes in developing cutting-edge software 
+            solutions for businesses of all sizes. Our team of experienced and talented developers, designers, and project 
+            managers are passionate about creating software that meets the unique needs of each and every client we work with.</p>
+             
+             <p className="lead">
+             At our core, we are problem solvers. We understand that technology is constantly evolving and businesses need to adapt quickly in order to stay ahead. That's why we work closely with our clients to understand their goals, 
+             challenges, and opportunities, so that we can deliver software solutions that help them achieve their objectives.
+             </p>
           </div>
         </div>
-      </div>
-      <section id="values" class="values  text-center"><div class="container" >
-        <header class="section-header"><h2>Our Strory</h2>
-          <p>Odit est perspiciatis laboruxtet dicta</p></header>
-        <div class="row"><div class="col-lg-4">
-          <div class="p-4 shadow"> <img src="/images/values-1.png" class="p-5 img-fluid" alt=""></img>
-            <h3 className="text-primary"> Our Humble Beginnings</h3><p>
-
-              Every entrepreneurial journey starts with inspiration from some source to someone and it takes a shape based on the vision and the passion of the team which comes together to accomplish their objective. Despite the fact that thousands of companies get started year on year, very few come to lime light and the glory of success, and accomplishment of objective.</p>
-          </div></div><div class="col-lg-4 mt-4 mt-lg-0">
-            <div class="p-4 shadow"> <img src="/images/values-2.png" class="p-5 img-fluid" alt=""></img>
-              <h3 className="text-primary">A Company is Born</h3>
-              <p>
-                We a small team of entrepreneurs kept working for a considerable time on understanding the fact that there is a certain way by which the success follows, hundreds of book, thousands of articles from experts and proven track of history, all this has formed the basis for our passion to work in a certain way to get through the success.</p></div></div>
-          <div class="col-lg-4 mt-4 mt-lg-0">
-            <div class="p-4 shadow"> <img src="/images/values-3.png" class="p-5 img-fluid" alt=""></img>
-              <h3 className="text-primary">Transition to Full Service.</h3>
-              <p>
-                This initiated us to start this company RR Softsol, which is robust with its passion to deliver quality solutions to our clients. We are too far from aspiring to be one of the leading companies, but every step we take forward; we keep our priority for a Happy Client List..</p></div></div></div></div></section>
-      <div className="container">
         <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-          <div className="col-lg-12 text-center">
-            <h3 className="text-crr">Our Values</h3>
-            <h1 className="display-3 fw-bold lh-sm mb-3">We follow these.</h1>
+          
+          <div className="col-lg-12"> 
+            <p className="lead">
+            Our company was founded on the principle of providing exceptional customer service. We believe that strong relationships with our clients are the key to success. That's why we pride ourselves on being responsive, flexible, and easy to work with. We strive to make
+             the software development process as seamless as possible, from the initial consultation to the final delivery of the product.
+              </p>
+             
+             <p className="lead">
+             We are committed to staying up-to-date with the latest technology trends and best practices. Our team is constantly learning and exploring new ways to improve our software development processes and deliver even better results to our clients. We believe that a commitment to innovation is essential to staying competitive in today's fast-paced business environment.
+
+              </p>
+              <p className="lead">
+              Our software solutions are designed to be scalable and customizable, ensuring that they can adapt to the changing needs of our clients over time. Whether you need a simple web application or a complex enterprise solution, we have the expertise and experience to deliver high-quality software that meets your requirements.
+
+              </p>
+              <p className="lead">
+              Thank you for considering our company for your software development needs. We look forward to the opportunity to work with you and help you achieve your business goals.
+
+              </p>
           </div>
         </div>
-
-        <div className="row row-cols-1 row-cols-sm-3 px-5 g-4 py-5 text-center">
-
-          {iconGRid.map((item) =>
-            <div className="col d-flex justify-content-center align-items-center">
-
-              <div>
-                <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="bi bi-headset" viewBox="0 0 16 16">
-                    <path d="M8 1a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V6a6 6 0 1 1 12 0v6a2.5 2.5 0 0 1-2.5 2.5H9.366a1 1 0 0 1-.866.5h-1a1 1 0 1 1 0-2h1a1 1 0 0 1 .866.5H11.5A1.5 1.5 0 0 0 13 12h-1a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h1V6a5 5 0 0 0-5-5z" />
-                  </svg>
-                </div>
-                <h4 className="fw-bold mb-0">{item.title}</h4>
-                <p>{item.desc}</p>
-              </div>
-            </div>
-          )
-          }
-
-        </div>
-      </div>
-
+      </div>  
 
     </Layout>
   )
